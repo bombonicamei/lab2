0347731a1f5d0644eb4b8b168fc92dd9e2302450
bombonicamei <122697984+bombonicamei@users.noreply.github.com>
2023-10-12 14:45:11 +0300

    Add files via upload

--- orientare.js
+++ orientare.js
@@ -1,21 +1,25 @@
 window.addEventListener("deviceorientation", on_device_orientation);
-function on_device_orientation(evt)
-{
-var alpha = evt.alpha;
-var beta = evt.beta;
-var gamma = evt.gamma;
 
-document.getElementById("a").innerHTML = "alpha = "+alpha;
-document.getElementById("b").innerHTML = "beta = "+beta;
-document.getElementById("c").innerHTML = "gamma = "+gamma;
+function on_device_orientation(evt) {
+  var alpha = evt.alpha;
+  var beta = evt.beta;
+  var gamma = evt.gamma;
 
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-var centru = {x:canvas.width/2, y:canvas.height/2}
-var latura_patrat = 50;
+  document.getElementById("a").innerHTML = "Alpha = " + alpha;
+  document.getElementById("b").innerHTML = "Beta = " + beta;
+  document.getElementById("c").innerHTML = "Gamma = " + gamma;
 
-context.clearRect(0, 0, canvas.width, canvas.height);
-context.fillStyle = "#6A0888";
-context.beginPath();
-context.fillRect(180, 150,evt.beta, 50);
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+  var centru = { x: canvas.width / 2, y: canvas.height / 2 };
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "#6A0888";
+
+  // Modificăm dimensiunea patratului în funcție de valoarea lui gamma
+  var latura_patrat = 50 + gamma; // Modifică acest factor pentru a ajusta dimensiunea
+  var x1 = centru.x - latura_patrat / 2;
+  var y1 = centru.y - latura_patrat / 2;
+
+  context.fillRect(x1, y1, latura_patrat, latura_patrat);
 }
